86ac791a6a43a2ced25d67f9b0032635a937fe38
majazimnoch <majamzimnoch@gmail.com>
2023-11-20 14:41:17 +0100

    configured tailwind according to the vercel documentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
+import Head from "next/head"
 export default function Home() {
   return (
-    <main className="px-6 mx-auto">
-      <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
-        Hello and welcome 👋
-        <span className="whitespace-nowrap">
-          I am <span className="font-bold">Maja</span>.
-        </span>
-      </p>
-    </main>
+    <div>
+      <Head>
+        <title>hdsaj</title>
+        <meta name="description" content="A simple Blog" />
+      </Head>
+      <main className="px-6 mx-auto">
+        <p className="mt-12 mb-12 text-3xl text-center dark:text-white">
+          Hello and welcome 👋
+          <span className="whitespace-nowrap">
+            I am <span className="font-bold">Maja</span>.
+          </span>
+        </p>
+      </main>
+    </div>
+
   )
 }
