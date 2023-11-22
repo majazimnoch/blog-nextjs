c9ce7e090bd92929070653f9b51002592d739099
majazimnoch <majamzimnoch@gmail.com>
2023-11-22 15:07:23 +0100

    won the battle with the navbar on top of the hero

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,32 +1,36 @@
-import React from 'react'
-import Image from 'next/image'
-import Round from './Round'
-import Navbar from './Navbar'
+import React from 'react';
+import Image from 'next/image';
+import Round from './Round';
+import Navbar from './Navbar';
 
 export default function Hero() {
   return (
-    <>
-      <Navbar />
-      <div>
-        <div className="w-screen h-screen">
-          <Image
-            src="/images/ocean.jpg"
-            alt="ocean"
-            layout="fill"
-            objectFit="cover"
-            style={{ opacity: 0.3 }} />
-        </div>
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center">
-          <p className="text-3xl text-center text-red-800">
-            Hello and welcoMMme 👋
-            <span className="text-red-400">
-              This is a <span className="font-bold">blog</span> made with
-              Next.js, Typescript and Tailwind.
-            </span>
-            <Round />
-          </p>
-        </div>
+    <div className="relative h-screen w-screen overflow-hidden">
+      {/* first we have div withe the image*/}
+      <div className="absolute inset-0">
+        <Image
+          src="/images/ocean.jpg"
+          alt="ocean"
+          layout="fill"
+          objectFit="cover"
+        />
       </div>
-    </>
-  )
+
+      <div className="absolute top-0 w-full z-50">
+        <Navbar />
+      </div>
+
+      {/* content is a child nr 2 */}
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-center z-10 w-full">
+        <p className="text-3xl text-center text-red-800">
+          Hello and welcoMMme 👋
+          <span className="text-red-400">
+            This is a <span className="font-bold">blog</span> made with
+            Next.js, Typescript, and Tailwind.
+          </span>
+          <Round />
+        </p>
+      </div>
+    </div>
+  );
 }
