f446cbf99ba29ea5fe500902aa1d11ed19538390
majazimnoch <majamzimnoch@gmail.com>
2023-11-22 18:18:32 +0100

    more styling

--- app/components/Intro.jsx
+++ app/components/Intro.jsx
@@ -5,10 +5,10 @@ import Button from './Button'
 
 export default function Intro() {
   return (
-    <div className="grid grid-cols-2 grid-rows-1 gap-x-8 gap-y-0 px-40 py-20">
+    <div className="grid grid-cols-2 grid-rows-1 gap-x-12 gap-y-0 px-40 py-20">
       <div className="flex flex-col gap-4">
         <h1 className="text-2xl">
-          A Seaside Serenity: An Unforgettable Family Getaway
+          <span className="bg-grass p-1 italic uppercase ">A Seaside Serenity.</span> An Unforgettable Family Getaway
         </h1>
         <p>
           Beneath the azure sky and the soothing sounds of crashing waves, our family embarked on an unforgettable journey to the sea. As the rhythmic tide pulled us into its embrace, we discovered the magic that only a seaside escape with loved ones could offer. Join us on a virtual voyage as we share the tales of our family adventure, where each sunset was a masterpiece and every wave carried the promise of new memories
@@ -21,12 +21,12 @@ export default function Intro() {
           />
         </Link>
       </div>
-      <div>
+      <div className="relative">
         <Image
           src="/images/mountains.jpg"
           alt="mountains"
-          height={250}
-          width={500}
+          layout="fill"
+          objectFit="cover"
         />
       </div>
     </div>
