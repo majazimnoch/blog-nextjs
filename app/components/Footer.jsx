79886e0401c95fa9445f9340585a67d71370c241
majazimnoch <majamzimnoch@gmail.com>
2023-11-23 17:19:28 +0100

    styling of the footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -1,23 +1,35 @@
 import React from 'react'
 import Image from 'next/image'
-
+import Link
+ from 'next/link'
 export default function Footer() {
   return (
     <div className="flex flex-row justify-center px-10 py-4 gap-40 items-center">
       <div className="flex flex-row gap-6 items-center">
-        <p className="bg-white text-black text-sm">Website made by Maja Zimnoch, intern at AKQA</p>
-        <Image
-          src="/images/git-icon.png"
-          alt="logo"
-          width={30}
-          height={30}
-          />
-        <Image
-          src="/images/li-icon.png"
-          alt="logo"
-          width={30}
-          height={30}
-          />
+        <p className="bg-white text-black text-sm uppercase tracking-normal font-thin">Website made by <span className="font-semibold">Maja Zimnoch</span>, intern at <span className="font-semibold">AKQA</span></p>
+
+        <div className="relative group over">
+        <Link href="https://github.com/majazimnoch" target="_blank">
+            <Image
+              src="/images/git-icon.png"
+              alt="logo"
+              width={30}
+              height={30}
+              className="transition-transform duration-300 ease-in-out transform scale-80 group-hover:scale-110"
+              />
+            </Link>
+        </div>
+        <div className="relative group over">
+          <Link href="https://www.linkedin.com/in/majazimnoch/" target="_blank">
+            <Image
+              src="/images/li-icon.png"
+              alt="logo"
+              width={30}
+              height={30}
+              className="transition-transform duration-300 ease-in-out transform scale-80 group-hover:scale-110"
+              />
+          </Link>
+        </div>
       </div>
       <Image
         src="/images/logo-blog.png"
