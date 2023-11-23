924c10a893e0023cc5b2a22c6f6b732a2cc3d8db
majazimnoch <majamzimnoch@gmail.com>
2023-11-23 15:18:05 +0100

    more styling of the group

--- app/components/Group.jsx
+++ app/components/Group.jsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 import clsx from 'clsx';
 
 // This component takes in several props that are passed to it when it's used elsewhere.
-const Article = ({ title, photo, text, articleClass, href }) => (
+const Article = ({ title, photo, text, articleClass, href, date }) => (
   <Link href={href}>
     { /*section below will have both the article class (for common styling among all articles) and the dynamic class specified by the articleClass prop (for specific styling for this individual article). */}
     <div className={clsx('article', articleClass, 'flex', 'flex-col', 'gap-10', 'justify-around')}>
@@ -21,6 +21,7 @@ const Article = ({ title, photo, text, articleClass, href }) => (
             />
           </div>
       </div>
+      <p className={clsx('text-xs')}>{date}</p>
       <p className="text-sm antialiased">{text}</p>
     </div>
   </Link>
@@ -37,6 +38,7 @@ export default function Group() {
           text="In the vast expanse of the desert, where time slows and silence reigns, I witnessed a spectacle that transcends earthly beauty—the desert sunset. Join me in this diary entry, a poetic reflection on the mesmerizing dance of colors as the sun bids adieu to the arid landscape, leaving behind a canvas painted in hues of warmth and wonder."
           articleClass="article-1"
           href="/posts/one"
+          date="1 January 2022"
         />
          <Article
           title="Montmartre Musings: Surreal Parisian Delights"
@@ -44,6 +46,7 @@ export default function Group() {
           text="In the heart of Paris, where cobblestone streets tell tales of artistry, Montmartre beckons—an enclave of creative magic. Join me in a chronicle of whimsical encounters with Dali, the enchanting lanes, and the irresistible allure of pastries that paint Paris in a palette of surreal delights."
           articleClass="article-2"
           href="/posts/two"
+          date="12 January 2022"
         />
          <Article
           title="Beneath the Surface: A Journal of Disconnecting Amidst Ocean Depths"
@@ -51,6 +54,7 @@ export default function Group() {
           text="In the hustle of modernity, a decision brewed—a weekend voyage to the enigmatic waters of Galápagos. Picture this as a diary, a chronicle of my plunge into underwater serenity, leaving behind the digital chatter for the immersive silence of the ocean."
           articleClass="article-3"
           href="/posts/three"
+          date="19 January 2022"
         />
          <Article
           title="Unplugged Bliss: A Digital Detox Weekend at the Spa"
@@ -58,6 +62,7 @@ export default function Group() {
           text="Welcome to a journey where serenity takes precedence and the hum of technology fades into the background. In a world marked by perpetual connectivity, the idea of escaping to a spa for a weekend without the intrusion of technology has become a beacon of tranquility."
           articleClass="article-4"
           href="/posts/four"
+          date="29 January 2022"
         />
     </div>
   )
