82a2644ecbd5004103a9e9c3fd73db326e7adebe
majazimnoch <majamzimnoch@gmail.com>
2023-11-24 12:09:38 +0100

    realized that its more tricky to build a form than i thought

--- app/posts/form/page.jsx
+++ app/posts/form/page.jsx
@@ -2,28 +2,33 @@ import Button from '@/app/components/Button'
 import Head from 'next/head'
 import Link from 'next/link'
 import React from 'react'
+import Navbar from '../../components/Navbar'
+import Group from '../../components/Group'
+import Footer from '../../components/Footer'
+import Form from '../../components/Form'
 
 export default function page() {
   return (
     <>
-    <Head>
-        <title> First post - blog page</title>
+      <Head>
+        <title> Form - blog page</title>
         <link rel="icon" href="favicon.ico" />
-    </Head>
-    <div>
-        <h1>
-       Form
-        </h1>
-        <h2>
-            <Link href="/">
-                <Button
-                label="Go back"
-                colour={"yellow"}
-                large
-                />
-            </Link> 
-        </h2>
-    </div>
+      </Head>
+      <div>
+        <Navbar />
+        <div className="flex flex-col gap-10 gap-x-12 gap-y-0 px-40 py-10">
+        <Form />
+          <Link href="/">
+            <Button
+              label="Go back"
+              colour={"yellow"}
+              large
+            />
+          </Link> 
+        </div>
+        <Group />
+        <Footer />
+      </div>
    </>
   )
 }
