c22a1c7ff012dfc3144d78cf803914877da95e7f
majazimnoch <majamzimnoch@gmail.com>
2023-11-24 12:24:23 +0100

    started making form in the form component

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function Form() {
   return (
-    <div>
-      Form
-    </div>
+    <form>
+        <div>
+            <label htmlFor="fullname">Full name</label>
+            <input type="text" id="email" placeholder="test@test.com" />
+        </div>
+
+        <div>
+            <label htmlFor="fullname">Full name</label>
+            <input type="text" id="email" placeholder="test@test.com" />
+        </div>
+
+        <div>
+            <label htmlFor="fullname">Full name</label>
+            <input type="text" id="email" placeholder="test@test.com" />
+        </div>
+        
+      {/* Form
+      https://www.youtube.com/watch?v=a2oa0qL4CB8 */}
+
+    </form>
   )
 }
--- app/posts/form/page.jsx
+++ app/posts/form/page.jsx
@@ -17,7 +17,11 @@ export default function page() {
       <div>
         <Navbar />
         <div className="flex flex-col gap-10 gap-x-12 gap-y-0 px-40 py-10">
-        <Form />
+        <div className="max-w-3xl mx-auto">
+          <h1 className="font-bold">Write a new blog post</h1>
+          <p>Please fill in the form</p>
+          <Form />
+        </div>
           <Link href="/">
             <Button
               label="Go back"
