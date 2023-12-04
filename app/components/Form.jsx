bfb3e18faf304714765c1b45b0841b35a4006c22
majazimnoch <majamzimnoch@gmail.com>
2023-12-04 11:55:09 +0100

    tes

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -4,25 +4,25 @@ import Image from 'next/image'
 
 export default function Form() {
   return (
-    <form className="min-w-full max-w-md mx-auto mt-4 p-10 bg-white shadow-md rounded-md bg-snow">
+    <form className="min-w-full max-w-md mx-auto mt-4 p-10 bg-white shadow-md bg-snow">
         <div className="flex flex-col">
-          <label htmlFor="fullname">Title of your blog post:</label>
+          <label htmlFor="fullname" className="py-6 bg-white text-black text-sm uppercase tracking-normal font-medium">Title of your blog post:</label>
           <input 
             type="text" 
             id="title" 
             placeholder="Amazing adventure"
-            className=""
+            className="resize-none px-3 py-2 focus:outline-none focus:ring focus:border-transparent-300"
           />
         </div>
 
         <div className="flex flex-col">
-            <label htmlFor="fullname">Type your words here:</label>
+            <label htmlFor="fullname" className="py-6 bg-white text-black text-sm uppercase tracking-normal font-medium">Type your words below:</label>
             <textarea
               id="content" 
               cols="2" 
               rows="20" 
               placeholder="Today was a good day." 
-              className="resize-none border rounded-md px-3 py-2 focus:outline-none focus:ring focus:border-transparent-300"
+              className="resize-none px-3 py-2 focus:outline-none focus:ring focus:border-transparent-300"
             />
         </div>
 
