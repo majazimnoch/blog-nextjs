4a8259b5722c813c7cd96523bd101ce3bf3ca76a
majazimnoch <majamzimnoch@gmail.com>
2023-12-04 10:16:42 +0100

    started working on form component

--- app/components/Form.jsx
+++ app/components/Form.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import Button from './Button'
 
 export default function Form() {
   return (
     <form>
         <div>
-            <label htmlFor="fullname">Full name</label>
+            <label htmlFor="fullname">Title of your blog post</label>
             <input type="text" id="email" placeholder="test@test.com" />
+        </div>
+
+        <div>
+            <label htmlFor="fullname">Type your words here</label>
+            <input type="text" id="email" cols="2" rows="30" placeholder="test@test.com" />
         </div>
 
         <div>
@@ -13,10 +19,11 @@ export default function Form() {
             <input type="text" id="email" placeholder="test@test.com" />
         </div>
 
-        <div>
-            <label htmlFor="fullname">Full name</label>
-            <input type="text" id="email" placeholder="test@test.com" />
-        </div>
+        <Button
+          label="Submit"
+          colour={"yellow"}
+          large
+        />
         
       {/* Form
       https://www.youtube.com/watch?v=a2oa0qL4CB8 */}
