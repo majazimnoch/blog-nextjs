8e52ce85e22ffa4b09aa6b33b05df4a861f1788b
majazimnoch <majamzimnoch@gmail.com>
2023-12-06 15:46:58 +0100

    added useEffect hook

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,6 +1,6 @@
 'use client' 
 
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
 import date from 'date-and-time';
 import Link from 'next/link';
@@ -11,19 +11,33 @@ const Navbar: React.FC = () => {
   const formattedDate = date.format(now, 'ddd, MMM DD, YYYY');
 
   const [isOpen, setIsOpen] = useState(false);
-
-  const isMobile = window.innerWidth < 768;
+  const [isMobile, setIsMobile] = useState(false);
 
   const handleClick = () => {
     setIsOpen(!isOpen);
   }
 
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    handleResize();
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+
   if (isMobile) {
     return (
-      <div className="bg-snow fixed top-0 right-0 p-10 w-full">
+      <div className="bg-snow flex flex-col justify-center items-right w-full p-10">
         <button
           onClick={handleClick}
-          className="flex flex-col justify-end items-end "
+          className="flex flex-col justify-end items-end"
         >
           <span
             className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
@@ -41,17 +55,11 @@ const Navbar: React.FC = () => {
             }`}
           ></span>
           {isOpen && (
-             <div className="flex flex-col gap-2 mt-2">
-              <Link href="/posts">
-                <a className="text-wood">User</a>
-              </Link>
-              <Link href="/posts/form">
-                <a className="text-wood">Add a new post</a>
-              </Link>
-              <Link href="/posts">
-                <a className="text-wood">Log out</a>
-              </Link>
-           </div>
+             <div className="flex flex-col gap-4 mt-2 text-center justify-start w-full underline underline-offset-8">
+              <Link href="/" className="flex flex-col text-center underline underline-offset-6">User</Link>
+              <Link href="/posts/form" className="flex flex-col text-center underline underline-offset-6" >Add a new post</Link>
+              <Link href="/posts/two" className="flex flex-col text-center underline underline-offset-6">Log out</Link>
+             </div>
           )}
         </button>
       </div>
