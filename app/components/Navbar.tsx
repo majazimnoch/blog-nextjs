e07b9ef9e356d600a548a4842053e20e028ca0bc
majazimnoch <majamzimnoch@gmail.com>
2023-12-06 16:10:07 +0100

    removed a comment

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -35,6 +35,7 @@ const Navbar: React.FC = () => {
   if (isMobile) {
     return (
       <div className="bg-snow flex flex-col justify-center items-right w-full p-10">
+        
         <button
           onClick={handleClick}
           className="flex flex-col justify-end items-end"
@@ -100,69 +101,3 @@ const Navbar: React.FC = () => {
 export default Navbar;
 
 
-
-
-
-
-// const Navbar: React.FC = () => {
-//   const now = new Date();
-//   const formattedDate = date.format(now, 'ddd, MMM DD, YYYY');
-
-//   const [isOpen, setIsOpen] = useState(false);
-
-//   const isMobile = window.innerWidth < 768;
-
-//   const handleClick = () => {
-//     setIsOpen(!isOpen);
-//   }
-
-//   if (isMobile) {
-//     return (
-//       <div className="bg-snow fixed top-0 right-0 p-10 w-full">
-//         <button 
-//           onClick={handleClick}
-//           className="flex flex-col justify-end items-end "
-//         >
-//           <span
-//             className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? 'rotate-45 translate-y-1' : '-translate-y-0.5'}`}></span>
-//           <span
-//             className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${isOpen ? 'opacity-0' : 'opacity-100'}`}></span>
-//           <span
-//             className={`bg-dark block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${isOpen ? '-rotate-45 -translate-y-1' : 'translate-y-0.5'}`}></span>
-//         </button>
-//       </div>
-//     );
-//   }
-
-//   return (
-//     <div className="flex flex-row justify-between items-center px-5 bg-snow">
-//       <div className="flex flex-row gap-6 items-center">
-//         <Link href="/">
-//           <Image
-//             src="/images/logo-b.png"
-//             alt="logo"
-//             width={30}
-//             height={30}
-//             className="rounded"
-//           />
-//         </Link>
-//         <p className="text-wood">
-//           Today&apos;s date: <span className="font-bold">{formattedDate}</span>
-//         </p>
-//       </div>
-//       <div className="flex flex-row gap-5">
-//         <Link href="/posts">
-//           <Button label="User" colour="yellow" large />
-//         </Link>
-//         <Link href="/posts/form">
-//           <Button label="Add a new post" colour="yellow" large />
-//         </Link>
-//         <Link href="/posts">
-//           <Button label="Log out" colour="yellow" large />
-//         </Link>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Navbar;
