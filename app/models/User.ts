b5bf99bd3c5bf188eeda00a0e61cf1b13a73a8eb
majazimnoch <majamzimnoch@gmail.com>
2024-01-02 15:08:05 +0100

    commented out error from models

--- app/models/User.ts
+++ app/models/User.ts
@@ -1,31 +1,31 @@
 import mongoose, { Document, Schema } from 'mongoose';
 
-interface IUser {
-    username: string;
-    email: string;
-    password: string;
-}
+// interface IUser {
+//     username: string;
+//     email: string;
+//     password: string;
+// }
 
-interface IUserDocument extends IUser, Document {
-}
+// interface IUserDocument extends IUser, Document {
+// }
 
-const UserSchema = new Schema<IUser>({
-    username: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    },
-}, { timestamps: true });
+// const UserSchema = new Schema<IUser>({
+//     username: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+//     email: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+//     password: {
+//         type: String,
+//         required: true,
+//     },
+// }, { timestamps: true });
 
-const UserModel = mongoose.models.User as mongoose.Model<IUserDocument> || mongoose.model<IUserDocument>('User', UserSchema);
+// const UserModel = mongoose.models.User as mongoose.Model<IUserDocument> || mongoose.model<IUserDocument>('User', UserSchema);
 
-export default UserModel;
+// export default UserModel;
